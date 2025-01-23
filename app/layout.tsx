eb30530255e6ddf562a827808b526bd3d09f2806
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2025-01-23 16:06:50 -0600

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,42 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const ibmSansPlex = localFont({
+  src: [
+    {
+      path: "./fonts/IBMPlexSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/IBMPlexSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const bebasNeue = localFont({
+  src: [
+    {
+      path: "./fonts/BebasNeue-Regular.ttf",
+      weight: "400",
+      style: "regular",
+    },
+  ],
+  variable: "--bebas-neue",
 });
 
 export const metadata: Metadata = {
